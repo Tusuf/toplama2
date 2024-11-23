6d86d1b0d4adaac67de93c0c7e08bc3145948a08
Tusuf <114356211+Tusuf@users.noreply.github.com>
2024-11-23 17:11:25 +0300

    ana dosya

---
+++ virus_baslat.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import time
+
+# Baslangicta bu mesaji veriyor isterseniz kaldirabilirsiniz
+print("LorNapes tarafından kodlandırılmıştır, like atıp abone olabilirsin.")
+time.sleep(5)
+
+# Buraya acacaginiz yazilimlarin adresini yazin varsayilan olarak toplama toplama yerini degistirin
+folder_path = os.path.join(os.environ['USERPROFILE'], 'Desktop', 'toplama')
+
+# Klasor var mi diye kontrol ediyor bu kisim onemli degistirmeyin
+if os.path.exists(folder_path):
+    try:
+        # Iterate through all files in the directory
+        for file in os.listdir(folder_path):
+            if file.endswith(".exe"):  # Check for .exe files
+                file_path = os.path.join(folder_path, file)
+                print(f"Executing: {file_path}")
+                subprocess.run([file_path], shell=True)  # Run the .exe file
+    except Exception as e:
+        print(f"An error occurred: {e}")
+else:
+    print(f"The folder does not exist: {folder_path}")
